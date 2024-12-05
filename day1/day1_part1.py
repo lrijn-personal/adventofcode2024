2e207b02a25a16c035a24b46942d9d5a1a0274da
lisa van rijn <lisa.van.rijn@enexis.nl>
2024-12-05 16:12:05 +0100

    add day 1

---
+++ day1/day1_part1.py
@@ -0,0 +1,24 @@
+file_path = "day1/input.txt"
+column1= []
+column2= []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        print(line)
+        int1, int2 = line.split()
+        column1.append(int(int1))
+        column2.append(int(int2))
+
+column1.sort()  
+column2.sort()
+
+total_distance = 0
+for i in range(len(column1)):
+    if column1[i] > column2[i]:
+        distance = column1[i] - column2[i]
+    else:
+        distance = column2[i] - column1[i]
+    # print(f"column1: {column1[i]}, column2: {column2[i]}, Distance: {distance}")
+    total_distance += distance
+
+print(f"Total distance: {total_distance}")
---
+++ day1/day1_part2.py
@@ -0,0 +1,18 @@
+file_path = "day1/input.txt"
+column1= []
+column2= []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        int1, int2 = line.split()
+        column1.append(int(int1))
+        column2.append(int(int2))
+
+score = 0
+for i in column1:
+    if i in column2:
+        count = column2.count(i)
+        score += i * count
+        print(f"number: {i}, count: {count}, score: {score}")
+
+print(f"Score: {score}")
